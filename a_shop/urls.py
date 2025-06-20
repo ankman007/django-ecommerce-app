475b989153ae9941d8c161332a0e0c08abd2f74f
ankman007 <ankeetpoudel6@gmail.com>
2025-06-20 10:19:37 +0545

    added-footer

--- a_shop/urls.py
+++ a_shop/urls.py
@@ -5,8 +5,12 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('cart/', cart, name='cart'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
+    path('privacy-policy/', privacy_policy, name='privacy_policy'),
     path('product/<str:id>', product_detail, name='product_detail'),
     path('category/<slug:slug>', category, name='category'),
+    path('cart/', cart, name='cart'),
+    path('add-to-cart/<str:product_id>/', add_to_cart, name='add_to_cart'),
 ]
 
--- a_shop/views.py
+++ a_shop/views.py
@@ -4,7 +4,8 @@ from .models import *
 from django.http import Http404
 from django.db.models import Count
 from loguru import logger
-
+from django.template.loader import render_to_string
+from django.http import HttpResponse
 
 def home(request):
     products = Product.objects.all()
@@ -15,6 +16,18 @@ def home(request):
         'categories': categories
     }
     return render(request, 'a_shop/home.html', context)
+
+
+def contact(request):
+    return render(request, 'a_shop/contact.html')
+
+
+def about(request):
+    return render(request, 'a_shop/about-us.html')
+
+
+def privacy_policy(request):
+    return render(request, 'a_shop/privacy-policy.html')
 
 
 def category(request, slug):
@@ -46,3 +59,29 @@ def cart(request):
         'products': products,
     }
     return render(request, 'a_shop/cart.html', context)
+
+
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from .models import Cart, CartItem
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    quantity = int(request.POST.get("quantity", 1))
+
+    cart, created = Cart.objects.get_or_create(user=request.user, is_active=True)
+    cart_item, item_created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product,
+        defaults={'quantity': quantity}
+    )
+    
+    if not item_created:
+        cart_item.quantity += quantity
+        cart_item.save()
+
+    cart_count = CartItem.objects.filter(cart=cart).count()
+    return JsonResponse({
+        "cart_count_html": render_to_string("a_shop/partials/cart_count.html", {"cart_count": cart_count}),
+        "button_html": render_to_string("a_shop/partials/add_to_cart_button.html", {"product_id": product_id})
+    })
